749b91b557f9cf69f972f265fbdf4ee564a96ec2
prakashthiya <77527384+prakashthiya@users.noreply.github.com>
2021-02-26 11:37:13 +0530

    c++

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float x,y,z;
+    cout<<"Enter two numbers";
+    cin>>x>>y;
+    z=x*y;
+    cout<<"product:"<<z;
+    return 0;
+}
